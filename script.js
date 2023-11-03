648f8268bffc38fe3906885828d6ad999d5efdaa
charline-studi <studi@charline-laporte.fr>
2023-11-03 16:37:06 +0100

    Ajout structure html / css pour le cours #1

---
+++ script.js
@@ -0,0 +1,17 @@
+/* 
+États de notre Tamastudi possibles :
+- 🥚 : partie non lancée) 
+- 🐣 : naissance pendant tant qu'il n'a pas fait son 1er caca
+Ensuite il devient un "grand" avec une humeur variable
+- 😢 : triste 0/5
+- 🙁 : pas content 1/5
+- 🙂 : normal 2/5
+- 😄 : content 3/5
+- 🤗 : heureux 4/5
+- 🥰 : très heureux 5/5
+- 👻 : mort 0/5 pendant plus d'une minute 
+Ses envies :
+- 😋 : faim, aléatoire minimum 30 sec et max 3 minutes
+- 🥱 : jouer, aléatoire minimum 30 sec et max 3 minutes
+- 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes après avoir mangé
+*/
