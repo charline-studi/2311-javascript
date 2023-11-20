f3443a90d2e9cb10131807669b1956e41e936a60
charline-studi <studi@charline-laporte.fr>
2023-11-20 11:20:53 +0100

    Ajout du cours #3

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ Ensuite il devient un "grand" avec une humeur variable
 Ses envies :
 - 😋 : faim, aléatoire minimum 30 sec et max 3 minutes
 - 🥱 : jouer, aléatoire minimum 30 sec et max 3 minutes
-- 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes après avoir mangé
+- 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes uniquement après avoir mangé
 */
 
 /* PHASE 0 : activer le tamastudi 
@@ -47,11 +47,10 @@ PHASE 1 : la naissance de mon tama
 */
 
 const birth = () => {
-  // demander le prénom
+  // 1) demander le prénom
   const tamaName = prompt("Quel nom a votre tamastudi ?");
   // 2) fait éclore mon oeuf pour passer au poussin
-  const character = document.querySelector(".js-character");
-  character.textContent = "🐣";
+  showInScreen("🐣");
   // 3) affiche mes vitals
   const vitals = document.querySelector(".js-vitals");
   vitals.classList.remove("hidden");
@@ -66,15 +65,69 @@ const birth = () => {
   // 6) afficher les actions
   const actions = document.querySelector(".js-actions");
   actions.classList.remove("hidden");
+  // 7) appel de la fonction pour le faire "grandir"
+  evolve();
 };
 
 /* PHASE 2 : l'évolution de mon tama
 1) Attendre que notre tamaStudi ait une "première envie"
 2) Il devient grand
 */
-setTimeout(() => {
-  console.log("1 seconde est passée");
-}, 1000);
+const evolve = () => {
+  // 1) Attendre que notre tamaStudi ait une "première envie"
+  const functionToExecute = () => {
+    showInScreen("🥰");
+  };
+  wantsTo(functionToExecute);
+};
+
+/* LES ENVIES : 
+Fonction pour gérer 
+- 😋 : faim, aléatoire minimum 30 sec et max 3 minutes
+- 🥱 : jouer, aléatoire minimum 30 sec et max 3 minutes
+- 💩 : caca, aléatoire minimum 30 sec et max 3 minutes uniquement après avoir mangé
+1) Créer une fonction qu'on va pouvoir appeler plus tard dans notre code
+2) Stocker les envies de mon tama dans une variable
+3) Avec un setTimeout choisir une envie aléatoire
+4) La durée du setTimeout est dynamique est comprise entre une valeur max et une valeur min
+5) Afficher l'envie du tama sur notre écran 
+6) L'envie de faire caca ne peut être faite que s'il a déjà mangé
+*/
+
+const wantsTo = (callback) => {
+  const needs = ["😋", "🥱", "💩"];
+  const minDuration = 1000;
+  const maxDuration = 3000;
+  const duration = getRandomInt({
+    min: minDuration,
+    max: maxDuration,
+  });
+  setTimeout(() => {
+    const randomIndexNeeds = getRandomInt({
+      max: needs.length,
+    });
+    const desire = needs[randomIndexNeeds];
+    if (callback) {
+      callback();
+    } else {
+      showInScreen(desire);
+    }
+  }, duration);
+};
+
+/* Fonction du retourne un nombre aléatoire compris entre un min et max */
+const getRandomInt = (props) => {
+  const max = props.max;
+  // Ternaire = condition "raccourcie" = condition ? valeur si oui : valeur si non
+  const min = props.min ? props.min : 0;
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+/* Fonction qui gère l'affichage des emoticones dans l'écran du tama */
+const character = document.querySelector(".js-character");
+const showInScreen = (display) => {
+  character.textContent = display;
+};
 
 // Lance la fonction de "début de mon Tama"
 start();
